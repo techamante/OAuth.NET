bd5bc485d8db5e5ec6e2ac2a65f9dcd5c682e436
Anuj Malla <anujmalla@purplequantum.com>
2016-08-06 23:45:24 -0700

    Initial check in

---
+++ OAuth2.Core.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace OAuth2.Core.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
